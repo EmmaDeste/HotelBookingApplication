dcc3beb5e64c2b462584907a18b4dce2bc65fc14
EmmaDeste <36630854+EmmaDeste@users.noreply.github.com>
2022-11-18 23:12:44 +0100

    Update Enquiry page: comments + indentation
    Add comments for each line or block of code 
    + Perfect indentations
    TODO: finish to add comments to the JS part

--- src/views/Enquiry.vue
+++ src/views/Enquiry.vue
@@ -1,92 +1,102 @@
 <template>
     <body>
         <header>
-        <nav> 
-            <img id="logo" src="../assets/logo.jpg"> 
-            <ul class="menu"> 
-                <li> <p v-on:click=link_home()> <span id="home"></span></p></li>
-                    <li> <p v-on:click=link_rooms()> <span id="rooms"></span></p></li>
-                    <li> <p v-on:click=link_booking()> <span id="booking"></span></p></li>
-                    <li> <p v-on:click=link_restaurant()> <span id="restaurant"></span></p></li>
-                    <li> <p v-on:click=link_about()> <span id="about"></span></p></li>
-                    <li> <p><span id="enquiries"></span></p></li>
-                    <li> <p v-on:click=link_feedbacks()> <span id="feedback"></span></p></li>
-                    <li> <p v-on:click=link_contact()> <span id="contact"></span></p></li>
-                </ul>
-                <br>
-                <br>
-                <ul> 
-                    <li class="roll"> <span id="language"></span> <img id="world" src="../assets/world.jpg">
-                        <ul class="options"> 
-                            <li id="af" v-on:click=afrikaans()> Afrikaans </li>
-                            <li id="eng" v-on:click=english()> English </li>
-                            <li id="fr" v-on:click=francais()> French </li>
-                            <li id="ge" v-on:click=deutsch()> Deutsch </li>
-                            <li id="es" v-on:click=espanol()> Español </li>
-                        </ul> 
-                    </li>
-                </ul>
-            </nav> 
-    </header>
-            <h1> <span id="know"></span> </h1>
-            <br> 
+			<!-- add a navigation bar, as the header of each page -->
+			<nav> 
+				<!-- logo of the hotel -->
+				<img id="logo" src="../assets/logo.jpg"> 
+
+				<!--  Menu of the navigation bar, constituted of the links to the other pages-->
+				<ul class="menu"> 
+					<li> <p v-on:click=(link_home(),english())> <span id="home"></span></p></li>
+						<li> <p v-on:click=link_rooms()> <span id="rooms"></span></p></li>
+						<li> <p v-on:click=link_booking()> <span id="booking"></span></p></li>
+						<li> <p v-on:click=link_restaurant()> <span id="restaurant"></span></p></li>
+						<li> <p v-on:click=link_about()> <span id="about"></span></p></li>
+						<li> <p><span id="enquiries"></span></p></li>
+						<li> <p v-on:click=link_feedbacks()> <span id="feedback"></span></p></li>
+						<li> <p v-on:click=link_contact()> <span id="contact"></span></p></li>
+					</ul>
+					<br>
+					<br>
+					<ul> 
+						<!-- Scolling bar to select the language -->
+						<li class="roll"> <span id="language"></span> <img id="world" src="../assets/world.jpg">
+							<!-- Options of language to read -->
+							<ul class="options"> 
+								<li id="af" v-on:click=afrikaans()> Afrikaans </li>
+								<li id="eng" v-on:click=english()> English </li>
+								<li id="fr" v-on:click=francais()> French </li>
+								<li id="ge" v-on:click=deutsch()> Deutsch </li>
+								<li id="es" v-on:click=espanol()> Español </li>
+							</ul> 
+						</li>
+					</ul>
+				</nav> 
+		</header>
             
-            <form class="review-form">
-                <h3> <span id="coord"></span>: </h3>
-                <br>
-                <label for="name"> <span id="name"></span>: </label>
-                <input v-model="name"> 
-                <br><br>
-                <label for="password"> <span id="password"></span>: </label>
-                <input v-model="password"> 
-
-                <br><br><br>
-
-                <h3> <span id="book"></span>: </h3>
-                <br>
-                <label for="checkinD"> <span id="inD"></span>: </label>
-                <input v-model="checkinD"> 
-                <br><br>
-                <label for="checkoutD"> <span id="outD"></span>: </label>
-                <input v-model="checkoutD"> 
-
-                <br><br>
-
-                <h3> <span id="type"></span>: </h3>
-                <br>
-                <label for="enqui"></label>
-                <select v-model="enqui">
-                    <option> <span id="billing"></span> </option>
-                    <option> <span id="stay"></span> </option>
-                    <option> <span id="serv"></span> </option>
-                    <option> <span id="other"></span> </option>
-                </select>
-
-                <br><br>
-
-                <h3> <span id="message"></span> </h3>
-                <br>
-                <label for="comment"></label>
-                <input v-model="comment"> 
-                
-            </form>
+		<h1> <span id="know"></span> </h1>
+        <br> 
             
+        <form class="review-form">
+             <h3> <span id="coord"></span>: </h3>
             <br>
+            <label for="name"> <span id="name"></span>: </label>
+            <input v-model="name"> 
+            <br><br>
+            <label for="password"> <span id="password"></span>: </label>
+            <input v-model="password"> 
 
-            <button id="listButton" v-on:click=listReviews()> SUBMIT </button>
-            
             <br><br><br>
 
-            <div readonly id="review_container" v-for="item in reviews">
-                <b1>{{item.name}}</b1> : <b2>{{item.checkinD}}</b2> - <b2>{{item.checkoutD}}</b2>
-                : <b3>{{item.comment}}</b3>
-            </div>
+            <h3> <span id="book"></span>: </h3>
+            <br>
+            <label for="checkinD"> <span id="inD"></span>: </label>
+            <input v-model="checkinD"> 
+            <br><br>
+            <label for="checkoutD"> <span id="outD"></span>: </label>
+            <input v-model="checkoutD"> 
 
-            <br> <br> 
+            <br><br>
+
+            <h3> <span id="type"></span>: </h3>
+            <br>
+            <label for="enqui"></label>
+             <select v-model="enqui">
+                <option> <span id="billing"></span> </option>
+                <option> <span id="stay"></span> </option>
+                <option> <span id="serv"></span> </option>
+                <option> <span id="other"></span> </option>
+            </select>
+
+            <br><br>
+
+        	<h3> <span id="message"></span> </h3>
+            <br>
+            <label for="comment"></label>
+            <input v-model="comment"> 
+                            
+		</form>
+            
+        <br>
+
+        <button id="listButton" v-on:click=listReviews()> SUBMIT </button>
+            
+        <br><br><br>
+
+        <div readonly id="review_container" v-for="item in reviews">
+            <b1>{{item.name}}</b1> : <b2>{{item.checkinD}}</b2> - <b2>{{item.checkoutD}}</b2>
+            : <b3>{{item.comment}}</b3>
+        </div>
+
+        <br> <br> 
 
     <footer>
+		<!-- indicate our social medias, as the footer of each page -->
         <p> <b> <span id="follow"></span> </b> </p>
         <br>
+
+		<!-- table listing the different social medias, taking the logos from the assets folder -->
         <table class="socialm"> 
             <tr> 
                 <td> <img id="fb" src="../assets/facebook.png">  </td>
@@ -123,17 +133,23 @@
 
 <script>
   export default{
+	//set the identifier name of this page, used in the router file
     name:'enquiry',
+	
     data(){return {
+		//set the variables from the form to the default value (prevent for errors: about null objects)
         name: '',
         password: '',
         checkinD: '',
         checkoutD: '',
         enqui: '',
         comment: '',
+		//array where the information from the form will be stored
         reviews: [],
         
+		//set the default language to english, which identifier is 1
         nLangue:1, 
+		//setup the list of all the translations used on this page
         listTradHome:["TUIS","HOME","ACCUEIL","HOME","HOME"],
         listTradRooms:["KAMERS","ROOMS","CHAMBRES","ZIMMERS","CUARTO"],
         listTradBooking:["BESPREKING","BOOKING","RÉSERVER","BUCHUNG","RESERVAR"],
@@ -159,106 +175,119 @@
         listTradFollow:["Volg ons:","Follow us:","Suivez-nous:","Folge uns:","Síganos:"],
     }},
     methods:{
-      link_about: function(event){
-        this.$router.push({path: '/about'})
-      },
-      link_booking: function(event){
-        this.$router.push({path: '/booking'})
-      },
-      link_home: function(event){
-        this.$router.push({path: '/'})
-      },
-      link_contact: function(event){
-        this.$router.push({path: '/contact'})
-      },
-      link_restaurant: function(event){
-        this.$router.push({path: '/restaurant'})
-      },
-      link_rooms: function(event){
-        this.$router.push({path: '/rooms'})
-      },
-      link_feedbacks: function(event){
-        this.$router.push({path: '/feedbacks'})
-      },
-      francais: function(event){
-            this.nLangue=2;
-            console.log("fr");
-            this.trad();
-        },
-        afrikaans: function(event){
-            this.nLangue=0;
-                console.log("af");
-                this.trad();
-            },
-            english:function(event){
-                this.nLangue=1;
-                console.log("eng");
-                this.trad();
-            },
-            deutsch:function(event){
-                this.nLangue=3;
-                console.log("ge");
-                this.trad();
-            },
-            espanol:function(event){
-                this.nLangue=4;
-                console.log("es");
-                this.trad();
-            },          
-            trad:function(event){
-                document.getElementById("home").innerHTML = this.listTradHome[this.nLangue];
-                document.getElementById("rooms").innerHTML = this.listTradRooms[this.nLangue];
-                document.getElementById("booking").innerHTML = this.listTradBooking[this.nLangue];
-                document.getElementById("restaurant").innerHTML = this.listTradRestaurant[this.nLangue];
-                document.getElementById("about").innerHTML = this.listTradAbout[this.nLangue];
-                document.getElementById("enquiries").innerHTML = this.listTradEnquiries[this.nLangue];
-                document.getElementById("feedback").innerHTML = this.listTradFeedbacks[this.nLangue];
-                document.getElementById("contact").innerHTML = this.listTradContact[this.nLangue];
-                document.getElementById("language").innerHTML = this.listTradLanguage[this.nLangue];
+		//ROUTING FUNCTIONS
+    	link_about: function(event){
+        	this.$router.push({path: '/home'})
+    	},
+    	link_booking: function(event){
+        	this.$router.push({path: '/booking'})
+    	},
+    	link_home: function(event){
+        	this.$router.push({path: '/'})
+    	},
+		link_contact: function(event){
+        	this.$router.push({path: '/contact'})
+    	},
+    	link_restaurant: function(event){
+        	this.$router.push({path: '/restaurant'})
+    	},
+    	link_rooms: function(event){
+        	this.$router.push({path: '/rooms'})
+    	},
+    	link_feedbacks: function(event){
+        	this.$router.push({path: '/feedbacks'})
+    	},
 
-                document.getElementById("know").innerHTML = this.listTradKnow[this.nLangue];
-                document.getElementById("coord").innerHTML = this.listTradCoord[this.nLangue];
-                document.getElementById("name").innerHTML = this.listTradName[this.nLangue];
-                document.getElementById("password").innerHTML = this.listTradPassword[this.nLangue];
-                document.getElementById("book").innerHTML = this.listTradBook[this.nLangue];
-                document.getElementById("inD").innerHTML = this.listTradInD[this.nLangue];
-                document.getElementById("outD").innerHTML = this.listTradOutD[this.nLangue];
-                document.getElementById("type").innerHTML = this.listTradType[this.nLangue];
-                document.getElementById("billing").innerHTML = this.listTradBilling[this.nLangue];
-                document.getElementById("stay").innerHTML = this.listTradStay[this.nLangue];
-                document.getElementById("serv").innerHTML = this.listTradServ[this.nLangue];
-                document.getElementById("other").innerHTML = this.listTradOther[this.nLangue];
-                document.getElementById("message").innerHTML = this.listTradMessage[this.nLangue];
+		//LANGUAGE SETTINGS FUNCTIONS
+    	francais: function(event){
+			//todo
+        	this.nLangue=2;
+			//todo
+			console.log("fr");
+			//todo
+			this.trad();
+    	},
+		afrikaans: function(event){
+			this.nLangue=0;
+			console.log("af");
+			this.trad();
+		},
+		english:function(event){
+		this.nLangue=1;
+			console.log("eng");
+			this.trad();
+		},
+		deutsch:function(event){
+			this.nLangue=3;
+			console.log("ge");
+			this.trad();
+		},
+		espanol:function(event){
+			this.nLangue=4;
+			console.log("es");
+			this.trad();
+		},          
+		//TRANSLATIONS SETTINGS FUNCTION
+    	trad:function(event){
+			//todo
+			document.getElementById("home").innerHTML = this.listTradHome[this.nLangue];
+			document.getElementById("rooms").innerHTML = this.listTradRooms[this.nLangue];
+			document.getElementById("booking").innerHTML = this.listTradBooking[this.nLangue];
+			document.getElementById("restaurant").innerHTML = this.listTradRestaurant[this.nLangue];
+			document.getElementById("about").innerHTML = this.listTradAbout[this.nLangue];
+			document.getElementById("enquiries").innerHTML = this.listTradEnquiries[this.nLangue];
+			document.getElementById("feedback").innerHTML = this.listTradFeedbacks[this.nLangue];
+			document.getElementById("contact").innerHTML = this.listTradContact[this.nLangue];
+			document.getElementById("language").innerHTML = this.listTradLanguage[this.nLangue];
 
-                document.getElementById("follow").innerHTML = this.listTradFollow[this.nLangue];
-            },
+			document.getElementById("know").innerHTML = this.listTradKnow[this.nLangue];
+			document.getElementById("coord").innerHTML = this.listTradCoord[this.nLangue];
+			document.getElementById("name").innerHTML = this.listTradName[this.nLangue];
+			document.getElementById("password").innerHTML = this.listTradPassword[this.nLangue];
+			document.getElementById("book").innerHTML = this.listTradBook[this.nLangue];
+			document.getElementById("inD").innerHTML = this.listTradInD[this.nLangue];
+			document.getElementById("outD").innerHTML = this.listTradOutD[this.nLangue];
+			document.getElementById("type").innerHTML = this.listTradType[this.nLangue];
+			document.getElementById("billing").innerHTML = this.listTradBilling[this.nLangue];
+			document.getElementById("stay").innerHTML = this.listTradStay[this.nLangue];
+			document.getElementById("serv").innerHTML = this.listTradServ[this.nLangue];
+			document.getElementById("other").innerHTML = this.listTradOther[this.nLangue];
+			document.getElementById("message").innerHTML = this.listTradMessage[this.nLangue];
 
-            listReviews:function(event){
-                if (this.name != '' && this.password != '' && this.checkinD != '' && this.checkoutD != '' && this.comment != '' ){
-                    console.log("No empty boxes");
-                    this.reviews.push({name: this.name, checkinD: this.checkinD, checkoutD: this.checkoutD, comment: this.comment});
-                }
-                else{
-                    console.log("Info missing");
-                    alert("Information missing");
-                }
-            }
-            
-            
-            }}
+			document.getElementById("follow").innerHTML = this.listTradFollow[this.nLangue];
+    	},
+
+		//SUBMIT REVIEWS FUNCTIONS
+		listReviews:function(event){
+			//todo
+			if (this.name != '' && this.password != '' && this.checkinD != '' && this.checkoutD != '' && this.comment != '' ){
+				//todo
+				console.log("No empty boxes");
+				//todo
+				this.reviews.push({name: this.name, checkinD: this.checkinD, checkoutD: this.checkoutD, comment: this.comment});
+			}
+			//If there is no blank boxes
+			else{
+				console.log("Info missing");
+				//todo
+				alert("Information missing");
+			}
+    	}
+	}}
 </script>
 
 <style>
-b1{
-    font-weight: bold;
-    color: lightseagreen;
-}
-b2{
-    font-weight: bold;
-    color: green;
-}
-b3{
-    font-weight: bold;
-    color: orangered
-}
+	/* Styling the display of the clients' reviews */
+	b1{
+		font-weight: bold;
+		color: lightseagreen;
+	}
+	b2{
+		font-weight: bold;
+		color: green;
+	}
+	b3{
+		font-weight: bold;
+		color: orangered
+	}
 </style>
